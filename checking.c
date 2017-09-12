f8e75156024cdbc3218ee4c2223178f35b908657
ArunkumarMani98 <30829508+ArunkumarMani98@users.noreply.github.com>
2017-09-12 15:18:55 +0530

    Create checking.c

---
+++ checking.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("\nEnter the value..");
+scanf("%d",&a);
+if(a<0)
+{
+printf("\nThe given number is negative);
+}
+elseif(a>0)
+{
+printf("\nThe given number is positive");
+}
+else
+{
+printf("\nThe give number is zero");
+}
+}
