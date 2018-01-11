0d00048cc38b422ba5ac2cd8f55ccd7bfa31a26c
ArunkumarMani98 <30829508+ArunkumarMani98@users.noreply.github.com>
2018-01-11 16:32:20 +0530

    Create checknum.c

---
+++ checknum.c
@@ -0,0 +1,21 @@
+#include<stdio.h.
+#include<conio.h>
+void main()
+{
+int n;
+printf("Enter the number");
+scanf("%d",&n);
+if(n==0)
+{
+printf("Your number is zero");
+}
+else if(n>0)
+{
+printf("Your value is positive");
+}
+else
+{
+printf("Your value is negative'):
+}
+getch();
+}
