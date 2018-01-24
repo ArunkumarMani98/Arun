7839c8307a339b2723f744465f5426304dae996b
ArunkumarMani98 <30829508+ArunkumarMani98@users.noreply.github.com>
2018-01-24 21:56:20 +0530

    Create isdigit.c

---
+++ isdigit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<ctype.h>
+
+int main(void)
+{
+	char a;
+	printf("\nEnter the character: ");
+	scanf("%c",&a);
+	if(a>='0'&&a<='9')
+	{
+		printf("\nYES");
+	}
+	else
+	{
+		printf("\nNO!!!");
+	}
+	return 0;
+}
