8e17d322b783baa60a9b29d2e498fe95739d071d
ArunkumarMani98 <30829508+ArunkumarMani98@users.noreply.github.com>
2018-01-25 20:38:32 +0530

    Create Additions.c

---
+++ Additions.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a,b,c;
+	printf("\nEnter the two numbers : ");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	c=a+b;
+	printf("\nThe sum of %d and %d is %d",a,b,c);
+	return 0;
+}
