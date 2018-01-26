38d3d9f73a5c75d5404c23c7aa813fdd35746122
ArunkumarMani98 <30829508+ArunkumarMani98@users.noreply.github.com>
2018-01-26 15:11:55 +0530

    Create AP.c

---
+++ AP.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a1,b1,i,sum=0,d;
+	printf("\nEnter the initial value: ");
+	scanf("%d",&a1);
+	printf("\nEnter the difference: ");
+	scanf("%d",&d);
+	printf("\nEnter the end value: ");
+	scanf("%d",&b1);
+	for(i=a1;i<=b1;i=i+d)
+	{
+		sum+=i;
+	}
+	printf("\n%d\n",sum);
+	return 0;
+}
