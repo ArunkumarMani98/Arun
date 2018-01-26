8a2d2d93a6c826a6785498273c34e9a7d08779e3
ArunkumarMani98 <30829508+ArunkumarMani98@users.noreply.github.com>
2018-01-26 21:53:18 +0530

    Create oddK.c

---
+++ oddK.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n,a[100],b[100],r,k,i,c,j;
+	printf("\nEnter the size of the array: ");
+	scanf("%d",&n);
+	printf("\nEnter the k value : ");
+	scanf("%d",&k);
+	j=0;
+	printf("\nEnter the array elements: ");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	c=0;
+	for(i=0;i<n;i++)
+	{
+		if(a[i]!=0)
+		{
+			b[j]=a[i];
+			j++;
+		}
+	}
+	printf("\nThe %d odd number is %d .",k,b[k]);
+	return 0;
+}
