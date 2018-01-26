e4fe42106338a189df33778bb591e324223a763f
ArunkumarMani98 <30829508+ArunkumarMani98@users.noreply.github.com>
2018-01-26 15:06:05 +0530

    Create equ.c

---
+++ equ.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+	int n,m,o,tot;
+	printf("\nEnter the three numbers : ");
+	scanf("%d",&n);
+	scanf("%d",&m);
+	scanf("%d",&o);
+	tot=(n*m)/o;
+	printf("\nThe result is %d.",tot);
+return 0;
+}
