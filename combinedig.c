a26ca98e67de11d8026c3d85fd829e271818eaa4
ArunkumarMani98 <30829508+ArunkumarMani98@users.noreply.github.com>
2018-01-26 21:17:29 +0530

    Create combinedig.c

---
+++ combinedig.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int a,b;
+	printf("\nEnter the two numbers : ");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	printf("\nThe combination of %d and %d is %d%d.",a,b,a,b);
+	return 0;
+}
