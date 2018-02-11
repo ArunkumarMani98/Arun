d8f27545b447f7f2e04f12073774883fb1c224b5
ArunkumarMani98 <30829508+ArunkumarMani98@users.noreply.github.com>
2018-02-11 11:08:05 +0530

    Update countchange.c

--- countchange.c
+++ countchange.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
+#include<ctype.h>
 int main(void)
 {
-	char a[100],b[100],i,c=0;
+	char a[100],b[100];
+	int i,c=0,k;
 	printf("\nEnter the first string : ");
 	scanf("%s",&a);
 	printf("\nEnter the second string : ");
 	scanf("%s",&b);
-	for(i=0;a[i]!='\0';i++)
+	printf("\nEnter the changes : ");
+	scanf("%d",&k);
+	for(i=0;a[i]!='\0'&&b[i]!='\0';i++)
 	{
 		if((int)a[i]!=(int)b[i])
 		{
 			c=c+1;
 		}
 	}
-	printf("\nThere are %d changes.",c);
+	if(c==k)
+	{
+		printf("\nYES!!!");
+	}
+	else
+	{
+		printf("\nNO!!!");
+	}
 	return 0;
 }
