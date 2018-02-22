0b52395d8279b7a733c391aab2725ed7b52537b4
ArunkumarMani98 <30829508+ArunkumarMani98@users.noreply.github.com>
2018-02-22 17:00:18 +0530

    Create prog.c

---
+++ prog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(void)
+{
+	int p,a,l,w,i,j,pp,c=0;
+	printf("\nEnter the perimeter of Rectangle : ");
+	scanf("%d",&p);
+	printf("\nEnter the area of Rectangle : ");
+	scanf("%d",&a);
+	pp=p/2;
+	for(i=1;i<=a;i++)
+	{
+		for(j=1;j<=pp;j++)
+		{
+			if(i+j==pp&&i*j==a)
+			{
+				printf("\nYes!!!");
+				c+=1;
+				break;
+			}
+		}
+	}
+	if(c==0)
+	{
+		printf("\nNo!!!");
+	}
+	return 0;
+}
