ef6864e792fce416654deca411d6d2af75ff080d
ArunkumarMani98 <30829508+ArunkumarMani98@users.noreply.github.com>
2018-03-13 16:24:16 +0530

    Create player82.c

---
+++ c/player82.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+	int n,a[100],min,t,i,j;
+	printf("\nEnter the array elements : ");
+	scanf("%d",&n);
+	printf("\nEnter the array elements : ");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	min=a[0]&a[i];
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			t=a[i]&a[j];
+			if(t<min)
+			{
+				min=t;
+			}
+		}
+	}
+	printf("\nThe bitwise AND is %d.",min);
+}
