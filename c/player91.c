c01e6b3c5218244bb3e9da76708d2d2fcd7aaaae
ArunkumarMani98 <30829508+ArunkumarMani98@users.noreply.github.com>
2018-03-13 19:31:36 +0530

    Update player91.c

--- c/player91.c
+++ c/player91.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+void main()
+{
+    int num, remainder, base = 1, binary = 0;
+    scanf("%d", &num);
+    while (num > 0)
+    {
+        remainder = num % 2;
+        binary = binary + remainder * base;
+        num = num / 2;
+        base = base * 10;
+    }
+    printf("%d",binary);
+}
