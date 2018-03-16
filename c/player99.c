49e7610c2fbb386016337038e17aa79f92b124cf
ArunkumarMani98 <30829508+ArunkumarMani98@users.noreply.github.com>
2018-03-16 14:00:25 +0530

    Create player99.c

---
+++ c/player99.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	long int n,j=1,t,oct=0;
+	printf("\nEnter the binary number : ");
+	scanf("%ld",&n);
+	while(n!=0)
+	{
+		t=n%10;
+		oct=oct+t*j;
+		j=j*2;
+		n=n/10;
+	}
+	printf("\nThe Octal value is : %lo",oct);
+}
